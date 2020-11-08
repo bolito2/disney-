0f3f29e855455d613fbef7142b824fac60b0bfeb
bolito2 <bolito2hd@gmail.com>
2020-11-08 16:21:45 +0100

    Generate words from the layers

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import csv
 import numpy as np
+from layers import *
+
 
 # Letters supported by the algorithm
-letters = ' 0123456789abcdefghijklmnopqrstuvwxyz'
+letters = ' 0123456789abcdefghijklmnopqrstuvwxyz>'
 n_letters = len(letters)
+
+
+# Returns the one-hot vector encoding of any given letter, using the above dictionary
+def one_hot(character):
+    return np.array([character == letters[i] for i in range(n_letters)], dtype=np.int32)
+
 
 # Extract the strain names from the dataset
 with open('cannabis.csv', newline='') as csvfile:
@@ -17,16 +25,41 @@ with open('cannabis.csv', newline='') as csvfile:
     # First row is metadata so delete it
     cannabis_names = cannabis_names[1:]
 
-# Encode names in a one-hot way
-for name in cannabis_names:
-    name_oh = np.zeros((len(name), n_letters))
+# Encode names into a numpy array(as one hot matrices)
+X = []
 
+for name in cannabis_names:
     # Replace syphons with spaces
     name = name.replace('-', ' ').lower()
 
-    for i in range(len(name)):
-        character = name[i]
-        # One-hot encoding of very single character
-        letter_oh = [character == letters[i] for i in range(n_letters)]
-        name_oh[i] = letter_oh
+    # Add the end token to the name
+    name = name + '>'
 
+    # Get one-hot encoding of every character
+    name_oh = np.zeros((len(name), n_letters))
+    for i in range(len(name)):
+        name_oh[i] = one_hot(name[i])
+
+    X.append(name_oh)
+
+letter = 'o'
+gen_strain = ''
+
+rnn_cell = RNNCell()
+softmax = SoftMax()
+
+a = np.zeros((n_letters, 1))
+while letter != '>':
+    # Add last letter to the name of the strain
+    gen_strain += letter
+
+    # Forward-propagate one step
+    a, y = rnn_cell(a, one_hot(letter))
+
+    # Get the probabilities of choosing each character
+    probabilities = np.reshape(softmax(y), [-1])
+
+    letter_index = np.random.choice(n_letters, p=probabilities)
+    letter = letters[letter_index]
+
+print(gen_strain)
