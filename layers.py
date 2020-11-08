62ea1f2bb9438d33bd054f8a8c864806385ee13f
bolito2 <bolito2hd@gmail.com>
2020-11-08 16:21:27 +0100

    units have to be equal to the number of letters

--- layers.py
+++ layers.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 # Letters supported by the algorithm
-letters = ' 0123456789abcdefghijklmnopqrstuvwxyz'
+letters = ' 0123456789abcdefghijklmnopqrstuvwxyz>'
 n_letters = len(letters)
 
 
 class RNNCell:
-    def __init__(self, units):
+    def __init__(self):
         # Bias vector
-        self.b = np.zeros((units, 1))
+        self.b = np.zeros((n_letters, 1))
 
-        # Weights, take into account the last layer weights(units) and input weights(n_letters)
+        # Set the weights, for both the last layer activations and this layers inputs
         # They have to be randomly-initialized, unlike the bias vector
-        self.Wx = np.random.random((units, n_letters))
-        self.Wa = np.random.random((units, units))
+        self.Wx = np.random.random((n_letters, n_letters))
+        self.Wa = np.random.random((n_letters, n_letters))
 
     # Forward propagate one step
     def __call__(self, a_prev, x):
+        # Reshape x to a row vector in case it only has one dimension
+        x = np.reshape(x, [-1, 1])
+
         a = np.dot(self.Wa, a_prev) + np.dot(self.Wx, x) + self.b
         # Use hyperbolic tangent for activation
         y = np.tanh(a)
