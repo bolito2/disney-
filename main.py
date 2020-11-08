d4725bf3f7867f40c11bdcb566bd32506c73069c
bolito2 <bolito2hd@gmail.com>
2020-11-08 14:10:15 +0100

    one-hot encoding

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import csv
+import numpy as np
 
+# Letters supported by the algorithm
+letters = ' 0123456789abcdefghijklmnopqrstuvwxyz'
+n_letters = len(letters)
+
+# Extract the strain names from the dataset
 with open('cannabis.csv', newline='') as csvfile:
     cannabis_data = csv.reader(csvfile)
     cannabis_names = []
@@ -10,4 +16,17 @@ with open('cannabis.csv', newline='') as csvfile:
 
     # First row is metadata so delete it
     cannabis_names = cannabis_names[1:]
+
+# Encode names in a one-hot way
+for name in cannabis_names:
+    name_oh = np.zeros((len(name), n_letters))
+
+    # Replace syphons with spaces
+    name = name.replace('-', ' ').lower()
+
+    for i in range(len(name)):
+        character = name[i]
+        # One-hot encoding of very single character
+        letter_oh = [character == letters[i] for i in range(n_letters)]
+        name_oh[i] = letter_oh
 
